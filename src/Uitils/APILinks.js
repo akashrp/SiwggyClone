4255743f2d90071de3158424a7f4568c58b82f56
akash <akashrp68@gmail.com>
2023-12-14 21:18:53 +0530

    update api to work with cors limit

--- src/Uitils/APILinks.js
+++ src/Uitils/APILinks.js
@@ -1,10 +1,10 @@
 // export const  MAIN_PAGE_DATA_API="https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5642452&lng=73.7768511&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
  export const  MAIN_PAGE_DATA_API="https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?";
 
- export const  Restaurant_Image_API ="https://corsproxy.io/?https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+ export const  Restaurant_Image_API ="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
  export const Restaurant_Menu_API ="https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5642452&lng=73.7768511&restaurantId=";
- export const OfferIcon ="https://corsproxy.io/?https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_28,h_28/Store_Assets/Icons/OfferIconCart";
- export const Dish_Image_API =" https://corsproxy.io/?https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/";
+ export const OfferIcon ="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_28,h_28/Store_Assets/Icons/OfferIconCart";
+ export const Dish_Image_API =" https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/";
  export const UPDATE_LIST="https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/update";
  export const LOCATIONS_URL = "https://corsproxy.io/?https://www.swiggy.com/dapi/misc/place-autocomplete?input=";
  export const GET_LOCATION_CORTDINATE= "https://corsproxy.io/?https://www.swiggy.com/dapi/misc/address-recommend?place_id="
